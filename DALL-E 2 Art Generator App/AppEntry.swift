0ba89b2343cf7d1d3dd2b784494d0f2dd233eee0
Alex Ulanch <alexulanch@gmail.com>
2023-08-21 22:20:42 -0400

    Initial Commit

---
+++ DALL-E 2 Art Generator App/AppEntry.swift
@@ -0,0 +1,20 @@
+//
+//  DALL_E_2_Art_Generator_AppApp.swift
+//  DALL-E 2 Art Generator App
+//
+//  Created by Alex Ulanch on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct AppEntry: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .onAppear() {
+                    print(Bundle.main.infoDictionary?["API_KEY"] as? String)
+                }
+        }
+    }
+}
---
+++ DALL-E 2 Art Generator App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DALL-E 2 Art Generator App
+//
+//  Created by Alex Ulanch on 8/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
